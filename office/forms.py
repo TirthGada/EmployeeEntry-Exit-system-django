14adf63aada909d80e6f671b845414c6421ab8b4
TirthGada <118129263+TirthGada@users.noreply.github.com>
2023-07-23 01:27:19 +0530

    Delete office directory

--- office/forms.py
+++
@@ -1,9 +0,0 @@
-# office/forms.py
-
-from django import forms
-
-class EmployeeForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    employee_id = forms.CharField(max_length=10)
-
