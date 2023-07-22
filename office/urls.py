22de663cb22af582214de7ec1a87a151c79635ef
Tirth <tirth@Tirths-MacBook-Air.local>
2023-07-23 01:28:32 +0530

    Dashboard , designing , apis created

--- office/urls.py
+++ office/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('enter/', views.enter_employee, name='enter_employee'),
     path('exit/', views.exit_employee, name='exit_employee'),
+    path('create/',views.create_employee_profile,name='create_employee_profile'),
+    path('dashboard/',views.dashboard,name="dashboard"),
     # Add other URL patterns for your views here
 ]
--- office/views.py
+++ office/views.py
@@ -1,48 +1,45 @@
-# office/views.py
 
 from django.shortcuts import render, redirect
-from .models import Employee
-from datetime import datetime
-# office/forms.py
-
 from .forms import EmployeeForm
+from .models import Employee, EntryExitRecord
+from datetime import datetime
+
+from django.shortcuts import render, redirect
+from .forms import EmployeeForm
+from .models import Employee, EntryExitRecord
+from datetime import datetime
+
+# ...
 
 def enter_employee(request):
     if request.method == 'POST':
         form = EmployeeForm(request.POST)
 
         if form.is_valid():
-            first_name = request.POST['first_name']
-            last_name = request.POST['last_name']
-            employee_id = request.POST['employee_id']
+            employee_id = form.cleaned_data['employee_id']
 
-            # Check if an employee with the given ID exists
             try:
                 employee = Employee.objects.get(employee_id=employee_id)
             except Employee.DoesNotExist:
                 employee = None
 
-            entry_time = datetime.now()
-
             if employee:
-                # If the employee already has an exit_time recorded for today, only update entry_time
-                if employee.exit_time and employee.exit_time.date() == entry_time.date():
-                    employee.entry_time = entry_time
-                    employee.save()
-                else:
-                    employee.entry_time = entry_time
-                    employee.save()
+                entry_time = datetime.now()
+                print(f"Employee {employee_id} entered at {entry_time}")
+                # Create an EntryExitRecord for the employee with the entry time
+                EntryExitRecord.objects.create(employee=employee, entry_time=entry_time)
             else:
-                # Create a new employee with entry time
-                employee = Employee(first_name=first_name, last_name=last_name, employee_id=employee_id, entry_time=entry_time)
-                employee.save()
+                # Redirect to create_employee_profile view
+                return redirect('create_employee_profile')
 
-            return redirect('enter_employee')
+            return redirect('index')
 
     else:
         form = EmployeeForm()
 
     return render(request, 'office/enter_employee.html', {'form': form})
+
+# ...
 
 
 def exit_employee(request):
@@ -50,35 +47,74 @@ def exit_employee(request):
         form = EmployeeForm(request.POST)
 
         if form.is_valid():
-            employee_id = request.POST['employee_id']
+            employee_id = form.cleaned_data['employee_id']
 
-            # Check if an employee with the given ID exists
             try:
                 employee = Employee.objects.get(employee_id=employee_id)
             except Employee.DoesNotExist:
                 employee = None
 
-            exit_time = datetime.now()
-
             if employee:
-                # If the employee already has an entry_time recorded for today, only update exit_time
-                if employee.entry_time and employee.entry_time.date() == exit_time.date():
-                    employee.exit_time = exit_time
-                    employee.save()
-                else:
-                    employee.exit_time = exit_time
-                    employee.save()
+                exit_time = datetime.now()
+                # Retrieve the most recent EntryExitRecord for the employee and update the exit_time
+                entry_exit_record = EntryExitRecord.objects.filter(employee=employee).latest('entry_time')
+                entry_exit_record.exit_time = exit_time
+                entry_exit_record.save()
             else:
+                return redirect('create_employee_profile')
                 # Employee with the given ID does not exist
-                return render(request, 'office/exit_employee.html', {'form': form, 'error_message': 'Employee not found'})
 
-            return redirect('exit_employee')
+            return redirect('index')
 
     else:
         form = EmployeeForm()
 
     return render(request, 'office/exit_employee.html', {'form': form})
 
-
 def index(request):
     return render(request, 'office/index.html')
+
+def create_employee_profile(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+
+        if form.is_valid():
+            employee_id = form.cleaned_data['employee_id']
+
+            try:
+                employee = Employee.objects.get(employee_id=employee_id)
+            except Employee.DoesNotExist:
+                employee = None
+
+            if employee:
+                # Employee with the given ID already exists
+                return render(request, 'office/create_employee_profile.html', {'form': form, 'error_message': 'Employee with this ID already exists'})
+
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            entry_time = datetime.now()
+            exit_time = None
+
+            # Create a new employee with the provided details
+            Employee.objects.create(employee_id=employee_id, first_name=first_name, last_name=last_name)
+
+            return redirect('index')
+
+    else:
+        form = EmployeeForm()
+
+    return render(request, 'office/create_employee_profile.html', {'form': form})
+
+def dashboard(request):
+    if request.method == 'POST':
+        employee_id = request.POST.get('employee_id')
+
+        try:
+            employee = Employee.objects.get(employee_id=employee_id)
+            entry_exit_records = EntryExitRecord.objects.filter(employee=employee)
+        except Employee.DoesNotExist:
+            entry_exit_records = None
+
+        return render(request, 'office/dashboard.html', {'entry_exit_records': entry_exit_records,'employee':'employee'})
+
+    return render(request, 'office/dashboard.html')
